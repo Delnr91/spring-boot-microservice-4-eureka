e3d4c4fd63cf55e07c862c06a8f6dbb394522145
Daniel Núnez Rojas <invdesdelnr@outlook.com>
2024-12-23 13:06:36 -0300

    Eureka Run

---
+++ src/main/java/com/dani/spring_boot_microservice_4_eureka/SpringBootMicroservice4EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.dani.spring_boot_microservice_4_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringBootMicroservice4EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice4EurekaApplication.class, args);
+	}
+
+}
